59dfa319dced41557c4bfcf1a57e83f5279eba60
pawan2505 <pawanaktu@gmail.com>
2025-05-13 08:42:35 +0530

    reactjs

--- react02-intro-folder-app/src/components/MyComponent.jsx
+++ react02-intro-folder-app/src/components/MyComponent.jsx
@@ -1,12 +1,37 @@
-
+// 1. Create a simple function component
 function MyComponent() {
-    
-    return (
-        <div>
-            <h1>MyComponent</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, ipsa!</p>
-        </div>
-    )
+  // 2. Return JSX (HTML-like code)
+  return (
+    <div>
+      <h1>MyComponent</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </div>
+  );
 }
 
+// 3. Export the component to use it in other files
 export default MyComponent;
+
+/*
+  --------------------------------------------
+ Explanation
+  --------------------------------------------
+  
+  Component:
+     - This is a React function component.
+     - It returns some UI using JSX.
+  
+  JSX:
+     - Looks like HTML, but it’s written inside JavaScript.
+     - React uses it to show content on screen.
+  
+  HTML Tags:
+     - We used <div>, <h1>, and <p> tags to display heading and paragraph.
+  
+  Export:
+     - Using `export default`, we can use this component in other files (like App.js).
+  
+  Tip:
+     - Har React file ek chhota UI part banata hai.
+     - Sabse pehle function component banana seekhna important hai.
+  */
