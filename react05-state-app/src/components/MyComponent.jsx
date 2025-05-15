594d31208654fce0a46b19277644184f46724a6a
pawan2505 <pawanaktu@gmail.com>
2025-05-15 10:28:53 +0530

    state

---
+++ react05-state-app/src/components/MyComponent.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const MyComponent = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={increment}>Click</button>
+    </div>
+  );
+};
+
+export default MyComponent;
